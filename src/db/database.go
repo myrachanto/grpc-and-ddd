f07ad2b077c96b15a50caff96e6ec8f9ad79e0ff
Antonio myracho <26426455+myrachanto@users.noreply.github.com>
2024-09-04 09:32:35 +0300

    context

--- src/db/database.go
+++ src/db/database.go
@@ -3,16 +3,13 @@ package db
 import (
 	"context"
 	"fmt"
+	"time"
 
 	// "go.mongodb.org/mongo-driver/bson"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	// "github.com/myrachanto/asokomonolith/support"
-)
-
-var (
-	ctx = context.TODO()
 )
 
 func DbConnection() (*mongo.Database, error) {
@@ -22,6 +19,8 @@ func DbConnection() (*mongo.Database, error) {
 	// }
 	// clientOptions := options.Client().ApplyURI("mongodb://host.docker.internal:27017")//locally hosted db accessed by dockerized app
 	// clientOptions := options.Client().ApplyURI("mongodb://mongodb:27017") //dockerized docker compose  db accessed by dockerized app
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to the database")
